884d2840bf3935778ddc1e24d09a4d0a07c71c67
Josh McDannel <mcdannel@gmail.com>
2015-11-04 14:08:36 -0700

    implement user security

--- app/scripts/modules/users/directive.login.js
+++ app/scripts/modules/users/directive.login.js
@@ -6,8 +6,8 @@
     .module('dartapp.users')
     .directive('dsLogin', dsLogin);
 
-  dsLogin.$inject = ['AuthService', 'Utils', '$location'];
-  function dsLogin(AuthService, Utils, $location) {
+  dsLogin.$inject = ['AuthService', 'userService', 'Utils', '$location'];
+  function dsLogin(AuthService, userService, Utils, $location) {
     var directive = {
   		scope: {},
       templateUrl: Utils.view('users', 'directive.login'),
@@ -18,12 +18,20 @@
     return directive;
 
     function _link(scope, element, attrs) {
-      scope.githubLogin = _thirdPartyLogin('github');
+      scope.login = _login;
 
-      function _thirdPartyLogin(service) {
-        AuthService.requestLogin(service).then(function() {
-          $location.path('scores/list');
-        });
+      function _login(service) {
+        AuthService.requestLogin(service)
+          .then(function(authData) {
+            console.log('auth done', authData);
+            userService.createUser(authData).then(function() {
+              $location.path('scores/list');
+            });
+
+          })
+          .catch(function(error) {
+            console.log('Auth error', error);
+          });
       }
     }
   }
